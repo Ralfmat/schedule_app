3419f976832a4b57bfdd85fdeeb41163eb1d52f8
Ralfmat <mateuszwanda@outlook.com>
2024-03-12 11:02:45 +0100

    Connection between frontend and backend successful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import './App.css'
+import React from 'react';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
-    <div className='App'>
-      
+    <div>
+      <HelloWorld />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
